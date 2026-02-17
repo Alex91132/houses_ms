e2e0f2220484aabb3565b59f0cac76a04c6f8b1b
Aleksandr Fedorov <alexandr91132@gmail.com>
2026-02-17 14:38:29 +0700

    Базовая обработка данных

--- app/api/endpoints/houses.py
+++ app/api/endpoints/houses.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter
 
 
-houses_router = APIRouter()
+houses_router = APIRouter(prefix="/houses", tags=["houses"] )
 
-@houses_router.get("/houses")
-async def root():
-    return {"message" : "Hello World"}
+@houses_router.get("/")
+async def houses():
+    return {"message" : "Дома"}
 
 
-@houses_router.get("/houses/{name}")
-async def say_hello(name: str):
-    return {"message" : f"Hello {name}"}
+@houses_router.get("/{house_id}")
+async def house_detail(house_id: int):
+    return {"message" : f"Дом {house_id}"}
--- app/main.py
+++ app/main.py
@@ -4,4 +4,3 @@ from app.api.endpoints.houses import houses_router
 
 app = FastAPI()
 app.include_router(houses_router)
-
