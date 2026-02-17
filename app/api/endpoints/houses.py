5f8d7c0003e9516bbc7e8955b5a46beba579269a
Aleksandr Fedorov <alexandr91132@gmail.com>
2026-02-17 15:53:28 +0700

    Добавил первые схемы

--- app/api/endpoints/houses.py
+++ app/api/endpoints/houses.py
@@ -1,13 +1,23 @@
 from fastapi import APIRouter
+from fastapi import HTTPException
+
+from typing import List
+
+from app.data import houses_list
+from app.schemas.house import HouseDetailSchema, HouseItemSchema
+
+houses_router = APIRouter(prefix="/houses", tags=["houses"])
 
 
-houses_router = APIRouter(prefix="/houses", tags=["houses"] )
-
-@houses_router.get("/")
+@houses_router.get("/", response_model=List[HouseItemSchema])
 async def houses():
-    return {"message" : "Дома"}
+    return houses_list
 
 
-@houses_router.get("/{house_id}")
+@houses_router.get("/{house_id}", response_model=HouseDetailSchema)
 async def house_detail(house_id: int):
-    return {"message" : f"Дом {house_id}"}
+    for house in houses_list:
+        if house["id"] == house_id:
+            return house
+
+    raise HTTPException(status_code=404, detail="House not found")
